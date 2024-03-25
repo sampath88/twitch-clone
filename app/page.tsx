4d5119d032f5b9071bd6f5cdb79f71f9d530cce9
sampath88 <sampathreddy8896@gmail.com>
2024-03-25 20:15:32 +0530

    theme setup
    prisma setup with supabase postgres
    
    local tunnel  for webhooks using ngrok

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="max-w-[720px] flex flex-row justify-between items-center p-4 gap-4 mx-auto my-5 bg-gray-200">
+    <div className="max-w-[720px] flex flex-row justify-between items-center p-4 gap-4 mx-auto my-5 bg-black rounded">
       <h1 className="text-lg font-bold">Dashboard</h1>
       <UserButton afterSignOutUrl="/" />
     </div>
