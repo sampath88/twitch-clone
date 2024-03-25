16c403dd4a83cddf02fb50e57d7860cd7f367c26
sampath88 <sampathreddy8896@gmail.com>
2024-03-25 14:20:11 +0530

    done authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <Button size="lg" variant={"ghost"}>Click me</Button>
+  return (
+    <div className="max-w-[720px] flex flex-row justify-between items-center p-4 gap-4 mx-auto my-5 bg-gray-200">
+      <h1 className="text-lg font-bold">Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
